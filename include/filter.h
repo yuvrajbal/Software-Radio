d886184f79f1bca91af1572333f243169e0cae38
Evan Liang <71235843+EvanLiang72@users.noreply.github.com>
2022-03-21 18:26:18 -0400

    Update filter.h

--- include/filter.h
+++ include/filter.h
@@ -16,8 +16,8 @@ Ontario, Canada
 // declaration of a function prototypes
 void impulseResponseLPF(float, float, unsigned short int, std::vector<float> &);
 std::vector<float> slice(std::vector<float>, int, int);
-void convolveFIRinBlocks(float *, const std::vector<float> &, const std::vector<float> &, std::vector<float> &, float, const int, const int);
-void blockProcess(std::vector<float> &, const std::vector<float> &, const std::vector<float> &, float blockSize, std::vector<float> &, std::vector<float> &, std::vector<float> &, const int, const int);
+void convolveFIRinBlocks(float *, const std::vector<float> &, const std::vector<float> &, std::vector<float> &, float, const int);
+void blockProcess(std::vector<float> &, const std::vector<float> &, const std::vector<float> &, float, std::vector<float> &, std::vector<float> &, std::vector<float> &, const int);
 
 
 #endif // DY4_FILTER_H
