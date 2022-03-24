e584007d9cc16e09f04aa48383aed1e091fd4de2
Vito Xie <xiev1@mcmaster.ca>
2022-03-24 15:12:43 -0400

    mono0 test

--- src/project.cpp
+++ src/project.cpp
@@ -14,10 +14,7 @@ Ontario, Canada
 #include "iofunc.h"
 #include "logfunc.h"
 #include <cmath>
-void rfFrontEnd(std::vector<float> &FMDemodData, float RFFS, float IFFS,int BLOCK_SIZE,int rf_decim){
-
-
-	std::vector<float> block_data;
+void rfFrontEnd(std::vector<float> &mono_block, float RFFS, float IFFS,int BLOCK_SIZE,int rf_decim){
 	for( unsigned int block_id = 0; ; block_id++){
 		std::vector<float> block_data(BLOCK_SIZE);
 		// Temporarily using RFFS instead of BLOCK_SIZE
@@ -30,83 +27,51 @@ void rfFrontEnd(std::vector<float> &FMDemodData, float RFFS, float IFFS,int BLOC
 		std::cerr << "Read block" << block_id << "\n";
 	}
 
-
-	//Are these correct?
 	float Fc = 100000;	//RF cutoff 100Khz
 	unsigned short int num_taps = 101;
 
 	std::vector<float> h;
 	impulseResponseLPF(RFFS, Fc, num_taps, h);
 
-	std::vector<float> mono_block, mono_buffer;
-	mono_buffer.resize(BLOCK_SIZE);
-
-	//for(unsigned int i = 0; i < audio_data.size(); i += BLOCK_SIZE){
-		//blockProcess(mono_block, audio_data, h, mono_buffer.size(), mono_buffer,rf_decim);
-
-		//FMDemodData.clear();
-	//}
-
 	//int numDecim = RFFS/IFFS;
-
 	int mono0Decim = 5;
-	int block_count = 0;
-	std::vector<float> I_data(audio_data.size() / 2);
-	std::vector<float> Q_data(audio_data.size() / 2);
-	std::vector<float> I_block(BLOCK_SIZE);
-	std::vector<float> Q_block(BLOCK_SIZE);
-	std::vector<float> I(I_data.size() / 10);
-	std::vector<float> Q(Q_data.size() / 10);
+	std::vector<float> I_block(BLOCK_SIZE/2);
+	std::vector<float> Q_block(BLOCK_SIZE/2);
+	std::vector<float> I(I_block.size()/mono0Decim);
+	std::vector<float> Q(Q_block.size()/mono0Decim);
 	std::vector<float> i_state(num_taps-1);
 	std::vector<float> q_state(num_taps-1);
 	std::vector<float> state(num_taps-1);
 	std::vector<float> prev_state(2);
-	std::vector<float> fm_demod(I.size());
-	std::vector<float> mono_data(audio_data.size());
-	split_audio_into_channels(audio_data, I_data, Q_data);
+	std::vector<float> fm_demod(I_block.size());
+	std::vector<float> mono_data(I_block.size());
+	split_audio_into_channels(block_data, I_block, Q_block);
 
+	//I_block = slice(I_data, block_count*BLOCK_SIZE, (block_count + 1)*BLOCK_SIZE);
+	//Q_block = slice(Q_data, block_count*BLOCK_SIZE, (block_count + 1)*BLOCK_SIZE);
 
-	/*processing flow (process all blocks entirely, then output?? or while(read block > process  > output block)?)
-	---Is writing done in blocks or all data at once?
+	//Might have to change to filter single block
+	convolveFIRinBlocks(I, I_block, h, i_state, BLOCK_SIZE, rf_decim);
+	convolveFIRinBlocks(Q, Q_block, h, q_state, BLOCK_SIZE, rf_decim);
 
+	demod(fm_demod,I,Q,prev_state);
+	std::vector<float> audio(fm_demod.size());
+	std::vector<float> h2(num_taps);
+	//What is our FS? 240 K samples/sec ?
+	impulseResponseLPF(IFFS, 16000, num_taps, h2);
+	convolveFIRinBlocks(audio,fm_demod,h2,state,BLOCK_SIZE,mono0Decim);
 
-	How do we read the data from the file
-	how do we get size of the input audio file
-	*/
+	//Critical Segment
+	//mono_data.insert(mono_data.end(), mono_block.begin(), mono_block.end());
 
-
-		I_block = slice(I_data, block_count*BLOCK_SIZE, (block_count + 1)*BLOCK_SIZE);
-		Q_block = slice(Q_data, block_count*BLOCK_SIZE, (block_count + 1)*BLOCK_SIZE);
-
-		//Might have to change to filter single block
-		convolveFIRinBlocks(I, I_block, h, i_state, BLOCK_SIZE, 10);
-		convolveFIRinBlocks(Q, Q_block, h, q_state, BLOCK_SIZE, 10);
-
-		demod(fm_demod,I,Q,prev_state);
-
-		std::vector<float> h2(num_taps);
-		//What is our FS? 240 K samples/sec ?
-		impulseResponseLPF(IFFS, 16000, num_taps, h2);
-		convolveFIRinBlocks(mono_block,fm_demod,h2,state,BLOCK_SIZE,mono0Decim);
-
-		//Critical Segment
-		//mono_data.insert(mono_data.end(), mono_block.begin(), mono_block.end());
-
-
-		std::vector<float> audio();
-		for(unsigned int k = 0;k<mono_data.size();k++){
-			if(std::isnan(mono_data[k])) audio[k] = 0;
-			else audio[k] = static_cast<short int>(mono_data[k]*16384);
-		}
-		fwrite(&audio[0],sizeof(short int),audio.size(),stdout);
-
-		block_count+=1;
+	for(unsigned int k = 0;k<mono_data.size();k++){
+		if(std::isnan(mono_data[k])) audio[k] = 0;
+		else audio[k] = static_cast<short int>(mono_data[k]*16384);
 	}
+	fwrite(&audio[0],sizeof(short int),audio.size(),stdout);
 
-
-
+}
 void monoStereo(std::vector<float> FMDemodData, float RFFS, float IFFS, int BLOCK_SIZE){
-
 
 
 }
@@ -156,8 +121,8 @@ int main(int argc, char* argv[])
 	float IFFS = 240000;
 	float audioFS = 48000;
 	int rf_decim = 10;
-	int audio_decim;
-	int BLOCK_SIZE =  1024 * rf_decim * 2;
+	int audio_decim = 5;
+	int BLOCK_SIZE =  1024 * rf_decim * 2 * audio_decim;
 
 	// const std::string in_fname = "../data/iq_samples.raw";
 	// std::vector<float> block_data;
@@ -176,21 +141,12 @@ int main(int argc, char* argv[])
 	//std::vector<float> FMDemodData;
 	//rfFrontEnd(FMDemodData,RFFS,IFFS,BLOCK_SIZE,rf_decim);
 
-	std::vector<float> mono_data(audio_data.size());
-	rfFrontEnd(mono_data,RFFS,IFFS,BLOCK_SIZE,rf_decim);
+	std::vector<float> mono_block;
+	rfFrontEnd(mono_block,RFFS,IFFS,BLOCK_SIZE,rf_decim);
 
 	//monoStereo(FMDemodData,RFFS,IFFS,BLOCK_SIZE);
 
 	//RDS();
-	//std::vector<float> processed_data(BLOCK_SIZE);
-
-	//std::vector<float> audio(BLOCK_SIZE);
-	// std::vector<float> audio(RFFS);
-	// for(unsigned int k = 0;k<mono_data.size();k++){
-	// 	if(std::isnan(mono_data[k])) audio[k] = 0;
-	// 	else audio[k] = static_cast<short int>(mono_data[k]*16384);
-	// }
-	// fwrite(&audio[0],sizeof(short int),audio.size(),stdout);
 
 
 	return 0;
